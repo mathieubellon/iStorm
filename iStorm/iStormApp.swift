0685e3cfbc80b49e8264d948a2109d10101742ca
Mathieu <contact@mathieubellon.fr>
2022-04-30 18:04:19 +0200

    Initial Commit

---
+++ iStorm/iStormApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStormApp.swift
+//  iStorm
+//
+//  Created by Matthieu Bellon on 30/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iStormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
