3e2e1e067942bb574bbc925bb063547982cf5598
Mathieu <contact@mathieubellon.fr>
2022-04-30 18:21:35 +0200

    initial

--- iStorm/ContentView.swift
+++ iStorm/ContentView.swift
@@ -9,13 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem{
+                    Image(systemName: "house.circle.fill")
+                    Text("Home")
+                }
+            EventsView().tabItem{
+                Image(systemName: "list.dash")
+                Text("All Events")
+            }
+            FavoritesView().tabItem{
+                Image(systemName: "star.fill")
+                Text("My Fav")
+            }
+            ProfileView().tabItem{
+                Image(systemName: "person.circle")
+                Text("My profile")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+struct HomeView: View {
+    
+    
+    var body: some View {
+        VStack {
+            Color.red.ignoresSafeArea(edges: .top)
+            Text("Home View")
+            Spacer()
+        }
+    }
+}
+
+struct EventsView: View {
+    
+    
+    var body: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea(edges: .top)
+            Text("Events View")
+        }
+    }
+}
+
+struct FavoritesView: View {
+    
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.ignoresSafeArea(edges: .top)
+            Text("Favorites View")
+        }
+    }
+}
+
+struct ProfileView: View {
+    
+    
+    var body: some View {
+        ZStack {
+            Color.purple.ignoresSafeArea(edges: .top)
+            Text("Profile View")
+        }
     }
 }
